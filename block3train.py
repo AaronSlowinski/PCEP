e251154de3a9cc221de0171992f1e2eb005c2a3c
Aaron <Aaron.Slowinski@gmail.com>
2024-04-20 18:06:52 -0700

    train

--- block3train.py
+++ block3train.py
@@ -1,5 +1,6 @@
 train_speed={"flyingscotsman": 201, "TGV": 320, "shinkansen": 320}
 
-for train in train_speed.values():
-    print(str(train) [0],end="")
+for train in train_speed:
+    print(train[0],end="")
     
+#prints fts
---
+++ block3train2.py
@@ -0,0 +1,6 @@
+train_speed={"flyingscotsman": 201, "TGV": 320, "shinkansen": 320}
+
+for train in train_speed.values():
+    print(str(train) [0],end="")
+    
+#prints 233
